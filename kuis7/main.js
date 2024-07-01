31f396894bdebd31313cbf79bc938d10e287bd52
Wahyudi120505 <wahyutjg123@gmail.com>
2024-07-01 19:28:55 +0700

    .

---
+++ kuis7/main.js
@@ -0,0 +1,291 @@
+let product = [
+    {
+        id: 1,
+        name: "Produk 1",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 2,
+        name: "Produk 2",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 3,
+        name: "Produk 3",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 4,
+        name: "Produk 4",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 5,
+        name: "Produk 5",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 6,
+        name: "Produk 6",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 7,
+        name: "Produk 7",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 8,
+        name: "Produk 8",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 9,
+        name: "Produk 9",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 10,
+        name: "Produk 10",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 11,
+        name: "Produk 11",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 12,
+        name: "Produk 12",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 13,
+        name: "Produk 13",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    {
+        id: 14,
+        name: "Produk 14",
+        gambar: "https://www.bhphotovideo.com/images/images2500x2500/asus_1015e_ds03_10_1_notebook_computer_983610.jpg",
+        price: "IDR 5.000.000,00",
+        desc:[
+            "Processor: Intel Core i5",
+            "Memory: 8GB DDR4",
+            "Storage: 1TB SSD",
+            "Display: 15.6-inch FHD",
+            "Battery: 3-cell Li-Ion 45Wh",
+            "Operating System: Windows 10 Home"
+        ]
+    },
+    
+];
+
+let big = document.querySelector(".container")
+big.style.width = "100%"
+big.style.height = "100vh"
+big.style.gap = "40px"
+big.style.display = "grid"
+big.style.gridTemplateColumns = "1fr 1fr 1fr 1fr"
+big.style.gridGap = "25px 0"
+
+product.forEach((p) => {
+    let container = document.createElement("div");
+    container.style.backgroundColor = "white"
+    container.style.border = "2px solid red"
+    container.style.width = "300px"
+    container.style.height = "500px"
+    container.style.marginLeft = "10px"
+    container.style.display = "flex"
+    container.style.cursor = "pointer"
+    container.style.justifyContent = "center"
+    container.style.flexDirection = "column"
+    container.style.textAlign = "center"
+    container.style.borderRadius = "20px"
+    container.style.cursor = "pointer"
+    container.style.boxShadow = "0 0 20px black" 
+
+    let figure = document.createElement("figure")
+    let figcaption = document.createElement("figcaption")
+    let image = document.createElement("img")
+    image.src = p.gambar;
+    image.style.width = "250px"
+    image.style.height = "179px"
+    image.style.borderRadius = "20px"
+    let h1 = document.createElement("h1")
+    h1.textContent = p.name
+    let h2 = document.createElement("h2")
+    h2.textContent = p.price
+    h2.style.color = "red"
+    let buy = document.createElement("button")
+    buy.textContent = "beli sekarang"
+    let like = document.createElement("button")
+    like.textContent = "like"
+    like.style.margin = "0 10px";
+    let coment = document.createElement("button")
+    coment.textContent = "coment"
+    let ul = document.createElement("ul");
+    p.desc.forEach((d) => {
+        let li = document.createElement("li")
+        li.textContent = d
+        ul.appendChild(li)
+    })
+
+
+    buy.addEventListener("click" , () =>{
+        confirm('apakah anda akan membeli produk ini ?')
+    })
+    like.addEventListener("click" , () => {
+        alert('kamu menyukai produk ini')
+    })
+    coment.addEventListener("click" , () => {
+        prompt('apa pendapat anda tentang produk ini ?')
+    })
+
+    // like.addEventListener("mouseover" , () => {
+    //     like.style.backgroundColor = "blue"
+    // });
+    // like.addEventListener("mouseout" , () => {
+    //     like.style.backgroundColor = "red"
+    // });
+    // coment.addEventListener("mouseover" , () => {
+    //     like.style.backgroundColor = "blue"
+    // });
+    // coment.addEventListener("mouseout" , () => {
+    //     like.style.backgroundColor = "red"
+    // });
+    // buy.addEventListener("mouseover" , () => {
+    //     like.style.backgroundColor = "blue"
+    // });
+    // buy.addEventListener("mouseout" , () => {
+    //     like.style.backgroundColor = "red"
+    // });
+
+    figcaption.appendChild(h1)
+    figcaption.appendChild(h2)
+    figcaption.appendChild(ul)
+    figcaption.appendChild(buy)
+    figcaption.appendChild(like)
+    figcaption.appendChild(coment)
+    figure.appendChild(image)
+    figure.appendChild(figcaption)
+    container.appendChild(figure)
+    big.appendChild(container)
+})
